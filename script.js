1bd53104eedef6d2c0ffc0934276ea2997a2762d
Abdirahman Jama <mustalid21@gmail.com>
2023-08-21 23:35:01 -0500

    finished render display function and project added notes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(onReady);
-// storing point data in the variables below. 
+// Declaring initial HP and AP for storage.  
 let fungusHP = 100;
 let playerAP = 100;
-
+// Funciton to call when DOM is ready.
 function onReady() {
     console.log('jquery works');
-    // on the click of each individual button run function accordingly.
+    // Set individual event listeners for each attack button.
     $('.attack-btn.arcane-scepter').on('click', arcaneScepter);
     $('.attack-btn.entangle').on('click', entangle);
     $('.attack-btn.dragon-blade').on('click', dragonBlade);
@@ -13,7 +13,7 @@ function onReady() {
 
 
 }
-
+// Function to handle arcaneScepter attack.
 function arcaneScepter(event){
     // console.log('arcane scepter attack function works:');
     playerAP -= 12
@@ -21,6 +21,7 @@ function arcaneScepter(event){
     fungusHP -= 14
     console.log(fungusHP);
 
+    // Checking to see if AP and HP are less than 0: if so set to 0
     if (playerAP <= 0) {
         playerAP = 0;
     }
@@ -30,6 +31,7 @@ function arcaneScepter(event){
 
     render();
 }
+// Function to handle entangle attack.
 function entangle(event){
     // console.log('entangle attack function works:');
     playerAP -= 23
@@ -37,6 +39,7 @@ function entangle(event){
     fungusHP -= 9
     console.log(fungusHP);
 
+    // Checking to see if AP and HP are less than 0: if so set to 0
     if (playerAP <= 0) {
         playerAP = 0;
     }
@@ -47,13 +50,14 @@ function entangle(event){
     render();
     
 }
+// Function to handle dragonBlade attack.
 function dragonBlade(event){
     // console.log('dragon blade attack function works:');
     playerAP -= 38
     console.log(playerAP);
     fungusHP -= 47
     console.log(fungusHP);
-
+// Checking to see if AP and HP are less than 0: if so set to 0
     if (playerAP <= 0) {
         playerAP = 0;
     }
@@ -63,6 +67,7 @@ function dragonBlade(event){
     
     render();
 }
+// Function to handle starFire attack.
 function starFire(event){
     // console.log(' starfire attack function works:');
     playerAP -= 33
@@ -70,6 +75,7 @@ function starFire(event){
     fungusHP -= 25
     console.log(fungusHP);
 
+    // Checking to see if AP and HP are less than 0: if so set to 0
     if (playerAP <= 0) {
         playerAP = 0;
     }
@@ -81,10 +87,22 @@ function starFire(event){
 
 }
 
-
+// Function to update playerAP and fungusHP on DOM. 
 function render(){
     $('.ap-text').text(playerAP + ' AP');
     $('.hp-text').text(fungusHP + ' HP');
+
+    //
+    if (playerAP <= 0) {
+        // If playerAP is 0 or less, disable all attack buttons and add 'jump' class to the enemy
+        $('.freaky-fungus').addClass('jump');
+        $('.attack-btn').prop('disabled', true);
+    }
+    if (fungusHP <= 0) {
+        // If fungusHP is 0 or less, disable all attack buttons and add 'dead' class to the enemy
+        $('.freaky-fungus').addClass('dead');
+        $('.attack-btn').prop('disabled', true);
+    }
 }
 
 
